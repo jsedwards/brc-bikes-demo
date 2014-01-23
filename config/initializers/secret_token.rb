206ff42789d15582ecaffefab12fc720ac186343
Jeff Edwards <js_edwards@yahoo.com>
2014-01-23 14:38:11 -0800

    initial commit rails skeleton with pg, rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrcBikes::Application.config.secret_key_base = '4388c5c83c133337055ba16d80a36999290ae06c5e518d7c3271ad27a1b0352fb0581392f63058e7776a1e537aa98362e37ad7aef6addc70c809610d553421b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BrcBikes::Application.config.session_store :cookie_store, key: '_brc-bikes_session'
