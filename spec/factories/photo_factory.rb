dc49a77806cb0a1aed4765c94ce53c5f070b510a
Jeff Edwards <js_edwards@yahoo.com>
2014-01-23 20:04:51 -0800

    list photo titles on post show page

---
+++ spec/factories/photo_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do 
+  factory :photo do 
+    title 'Example photo'
+    description 'Just an example photo'
+  end
+end
---
+++ spec/features/viewing_photos_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "Viewing Photos" do 
+  before do
+    shiny_bikes = FactoryGirl.create(:post, title: 'New Bikes')
+    FactoryGirl.create(:photo, post: shiny_bikes, title: 'ohh, ahh', description: 'pretty, new bikes')
+    bike_sale = FactoryGirl.create(:post, title: 'Bike Sale')
+    FactoryGirl.create(:photo, post: bike_sale, title: 'lower prices', description: 'look at the good deals')
+
+    visit '/'
+  end
+
+  scenario "Viewing photos for a given post" do 
+    click_link 'New Bikes'
+
+    expect(page).to have_content('ohh, ahh')
+    expect(page).to_not have_content('lower prices')
+
+    click_link 'ohh, ahh'
+    within('#photo h2') do 
+      expect(page).to have_content('ohh, ahh')
+    end
+
+    expect(page).to have_content('pretty, new bikes')
+  end
+end
