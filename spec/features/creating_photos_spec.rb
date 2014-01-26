9b8145dee508448d52463b1179bc4b9689a6065f
Jeff Edwards <js_edwards@yahoo.com>
2014-01-25 21:30:46 -0800

    updated photo form to read either 'Add Photo' or 'Update Photo' depending if new photo

--- spec/features/creating_photos_spec.rb
+++ spec/features/creating_photos_spec.rb
@@ -12,13 +12,13 @@ feature "Adding Photos" do
   scenario "Adding a photo" do 
     fill_in "Title", with: "New Bike"
     fill_in "Description", with: "sweet ride"
-    click_button "Create Photo"
+    click_button "Add Photo"
 
     expect(page).to have_content("Photo has been added.")
   end
 
   scenario "Adding a photo without valid attributes fails." do 
-    click_button "Create Photo"
+    click_button "Add Photo"
 
     expect(page).to have_content("Photo has not been added.")
     expect(page).to have_content("Title can't be blank")
